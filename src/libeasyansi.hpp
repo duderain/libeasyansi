39c67f0dbb74133ed9c876aa1d40b5935b2de68a
Myles Rainsford <nieh444onscratch@gmail.com>
2026-05-19 19:59:44 +1000

    works on windows now

--- src/libeasyansi.hpp
+++ src/libeasyansi.hpp
@@ -2,12 +2,35 @@
 #include <iostream>
 #include <string>
 
-namespace libeasyansi {
-    inline std::ostream &ansi(std::ostream &os, const std::string code){
-        #ifdef _WIN32
-        return os << "`e[" << code << "m";
-        #else
-        return os << "\033[" << code << "m";
-        #endif
- }
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+namespace easyansi {
+
+    #ifdef _WIN32
+
+    struct windowsInitalizer { 
+        windowsInitalizer() {
+            HANDLE hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+            DWORD dwMode = 0;
+            if (hOut != INVALID_HANDLE_VALUE && GetConsoleMode(hOut, &dwMode)) {
+            dwMode |= ENABLE_VIRTUAL_TERMINAL_PROCESSING;
+            SetConsoleMode(hOut, dwMode);
+            }
+        }
+    };
+     easyansi::windowsInitalizer dummy;
+    #endif
+
+
+    struct ansi {
+        std::string code;
+        explicit ansi(std::string c) : code(std::move(c)) {}
+    };
+
+    inline std::ostream& operator<<(std::ostream& os, const ansi& a){
+        return os << "\033[" << a.code << "m";
+    }
+    
 }
